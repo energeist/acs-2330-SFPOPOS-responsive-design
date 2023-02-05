4b58a34576ab4ef8547a7fcacff9fbc8b8cef57e
Mark Rattle <mark.rattle@gmail.com>
2023-02-04 19:19:02 -0500

    POPOS Spaces styling

--- src/components/POPOSSpace.js
+++ src/components/POPOSSpace.js
@@ -1,25 +1,29 @@
 // src/POPOSSpace.js
-import './POPOSpace.css'
+import './POPOSSpace.css'
 import { Link } from 'react-router-dom'
 
 function POPOSSpace(props) {
   const { name, image, address, hours, id } = props;
   return (
-    <div className='POPOSpace'>
+    <div className='POPOSSpace'>
       <Link to={`/details/${id}`}>
         <img src={`${process.env.PUBLIC_URL}/images/${image}`}     
           width="300"
           height="300"
-          alt="50 Califonia St."
+          alt={name}
         />
       </Link>
       <h1>
-        <Link to={`/details/${id}`}>
+        <Link 
+          className="POPOSSpace-title"
+          to={`/details/${id}`}>
           {name}
         </Link>
       </h1>
-      <div>{address}</div>
-      <div className='hours'>{hours}</div>
+      <div className="POPOSSpace-info">
+        <div>{address}</div>
+        <div className='hours'>{hours}</div>
+      </div>
     </div>
   )
 }
